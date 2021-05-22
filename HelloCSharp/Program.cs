8f040f2011443c167282b0c5e26f674cf316e672
Rus Hoffman <RealQwazwak@gmail.com>
2021-05-21 22:00:01 -0500

    Add project files.

---
+++ HelloCSharp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                bool havePath = false;
+                Console.Write("No arguments were entered\nEnter the path to the BrainFuck source file: ");
+                while (havePath)
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        /* ConsoleKeyInfo key = Console.ReadKey(true);
+                         switch (key.Key)
+                         {
+                             case ConsoleKey.F1:
+                                 Console.WriteLine("You pressed F1!");
+                                 break;
+                             default:
+                                 break;
+                         }*/
+                        string input = Console.ReadLine();
+                    }
+                }
+
+                Console.WriteLine($"You are {input} years old!");
+            }
+            Console.Write("Hello\n");
+            Console.Write("2nd line");
+        }
+    }
+}
